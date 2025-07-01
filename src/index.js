74ad2d7c7170c361b3aa94f9cb17921980e1fb3d
Cristina Pérez <cristinaperezhuerta@gmail>
2025-07-01 12:20:12 +0200

    Configure server, implement functions for database connection, define routes for endpoints

--- src/index.js
+++ src/index.js
@@ -10,22 +10,87 @@ const server = express();
 server.use(cors());
 server.use(express.json({ limit: "25Mb" }));
 
+// Función para conectar con la base de datos
+
+async function getConnection() {
+  const connData = {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  };
+
+  const conn = await mysql.createConnection(connData);
+  await conn.connect();
+
+  return conn;
+}
+
 // Arrancamos el servidor en el puerto 4000
 const port = 4000;
 server.listen(port, () => {
   console.log(`🚀 El servidor está vivo en <http://localhost:${port}>`);
 });
 
-// EJEMPLO DE ENDPOINT
+// ENDPOINTS API
 
-server.get("/", (req, res) => {
-  console.log("Holis"); // -> Se ve en la terminal
+server.get("/api/frases", async (req, res) => {
+  // SELECT * - Listar todas las frases(con información del personaje y el título del capítulo)
 
-  res.send("Holis Adalabers!!!"); // -> Se ve en la página
+  // 1. Hacer las validaciones que consideremos
+
+  // 2. Conectarse a la bbdd
+  const conn = await getConnection();
+
+  // 3. Lanzar sentencia SELECT
+
+  // 4. Cerrar la conexión
+
+  // 5. Devolver un json como respuesta a la petición
+  //    o un objeto con el error
 });
 
-// SECCIÓN DE SERVIDOR DE APIS
+server.post("/api/frases", async (req, res) => {
+  // INSERT INTO -> DEVUELVE UN OBJETO CON SUCCESS Y EL ID/DATOS CREADOS O UN ERROR
+  // 1. Hacer las validaciones que consideremos
+  // 2. Conectarse a la bbdd
+  // 3. Lanzar sentencia INSERT
+  // 4. Cerrar la conexión
+  // 5. Devolver un json como respuesta a la petición (con el id generado)
+  //    o un objeto con el error
+});
 
-// SECCIÓN DE SERVIDOR DE FICHEROS DINÁMICOS
+server.get("/api/frases:id", (req, res) => {
+  // SELECT FROM id = - Obtener una frase específica
+  // DEVOLVER UN ARRAY DE OBJ
+});
 
-// SECCIÓN DE SERVIDOR DE FICHEROS ESTÁTICOS
+server.put("/api/frases/:id", async (req, res) => {
+  // UPDATE -> Actualizar una frase existente
+  // 1. Hacer las validaciones que consideremos
+  // 2. Conectarse a la bbdd
+  // 3. Lanzar sentencia UPDATE
+  // 4. Cerrar la conexión
+  // 5. Devolver un json como respuesta a la petición
+  //    o un objeto con el error
+  // {success: false, error: "" }
+  // {success: true, id:}
+});
+
+server.delete("/api/frases/:id", async (req, res) => {
+  // DELETE FROM -> Eliminar una frase
+  // 1. Hacer las validaciones que consideremos
+  // 2. Conectarse a la bbdd
+  // 3. Lanzar sentencia DELETE FROM
+  // 4. Cerrar la conexión
+  // 5. Devolver un json como respuesta a la petición
+  //    o un objeto con el error
+  // {success: false, error: "" }
+  // {success: true}
+});
+
+// ENDPOINT ERROR 404
+server.use(/.*/, (req, res) => {
+  res.status(404).send("Esa ruta no existe");
+});
